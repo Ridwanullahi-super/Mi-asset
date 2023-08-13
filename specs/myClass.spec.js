e0d46b751b58a42802748699a37d1b7f3998a27a
Ridwanullahi-super <asimiyuridwan5@gmail.com>
2023-08-13 01:27:12 +0100

    first commit

---
+++ specs/myClass.spec.js
@@ -0,0 +1,32 @@
+ var Myclass = require("../src/myClass")
+ const chai = require("chai")
+const chaiasPromise = require("chai-as-promised")
+ const sinon = require("sinon")
+ const expect = chai.expect;
+
+ var myObj = new Myclass()
+
+ describe("Test suite", async()=>{
+    await it("test method for add", async()=>{
+      await  expect(myObj.add(1,3)).to.be.equal(4)
+    })
+
+    await it("test method for add", async()=>{
+      const spy = sinon.spy(myObj,"add")
+      const arg1 = 23, arg2 = 25;
+      myObj.CallAnothFn(arg1,arg2);
+     await sinon.assert.calledOnce(spy)
+     await expect(spy.calledOnce).to.be.true;
+    })
+ })
+ describe("test the promise", function (){
+    it("promise test case", function (done){
+        this.timeout(0)
+    //     myObj.testPromise().then(function(result){
+    //         expect(result).to.be.equal(5)
+    //         // expect(false).to.be.true;
+    //         done();
+    //     })
+    return expect(myObj).testPromise().to.eventually.equal(6 )
+    })
+ })
