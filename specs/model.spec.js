c6546375216bed0ad9ff1fdd6350a37c77cee4a9
Ridwanullahi-super <asimiyuridwan5@gmail.com>
2023-08-13 03:31:59 +0100

    latest

--- specs/model.spec.js
+++ specs/model.spec.js
@@ -2,19 +2,42 @@ const model = require("../models/model")
 const mymodel = new model() 
 
 describe("test the save function", async()=>{
- await  it("test the save function", async()=>{
+   it("test the save function", async()=>{
         const spy = sinon.spy(mymodel,"save")
-        const arg1 = "test", arg2 = "test";
-        mymodel.save(arg1,arg2);
+        mymodel.save();
         await sinon.assert.calledOnce(spy)
         await expect(spy.calledOnce).to.be.true;
     })
 
     it("test the fetch function in the model", async()=>{
         const spy = sinon.spy(mymodel,"fetch")
-        const arg1 = "test", arg2 = "test";
-        mymodel.fetch(arg1,arg2);
+        const arg1 = "test";
+        mymodel.fetch(arg1);
         await sinon.assert.calledOnce(spy)
         await expect(spy.calledOnce).to.be.true;
     })
-})
+    it("test the delete function in the model", async()=>{
+        const spy = sinon.spy(mymodel,"delete")
+        mymodel.delete(arg1,arg2);
+        await sinon.assert.calledOnce(spy)
+        await expect(spy.calledOnce).to.be.true;
+    })
+    it("test the update function in the model", async()=>{
+        const spy = sinon.spy(mymodel,"update")
+    
+        // await sinon.assert.calledOnce(spy)
+        await expect( mymodel.update()).to.be.false;
+    })  
+    it("test the findId function in the model", async()=>{ 
+        const spy = sinon.spy(mymodel,"findId")
+        const arg1 = "test";
+        mymodel.findId(arg1);
+        await sinon.assert.calledOnce(spy)
+        await expect(spy.calledOnce).to.be.true;
+    })  
+   it("test tostrings function in the model", async()=>{   
+    const spy = sinon.spy(mymodel,"tostrings")     
+    mymodel.tostrings();
+    await sinon.assert.calledOnce(spy)
+    await expect(spy.calledOnce).to.be.true;
+})})
---
+++ specs/server.spec.js
@@ -0,0 +1,8 @@
+// const Mail = require("nodemailer/lib/mailer");
+// const mail = require("./mail/autoReminder");
+
+
+
+// decribe("test the entire server",async()=>{
+//     instanceof(
+// })
